f2c31e07844923bf33b8670075f71fdd9041aef0
Robert Jackson <rjackson@linkedin.com>
2017-06-20 22:22:49 -0400

    Initial project structure.

---
+++ README.md
@@ -0,0 +1,1 @@
+# babel-plugin-ember-modules-api-polyfill
---
+++ tests/index-test.js
@@ -0,0 +1,30 @@
+'use strict';
+/* globals QUnit */
+
+const describe = QUnit.module;
+const it = QUnit.test;
+
+const babel = require('babel-core');
+
+const Plugin = require('../src');
+
+function transform(source) {
+  let result = babel.transform(source, {
+    plugins: [
+      [Plugin],
+    ],
+  });
+
+  return result.code;
+}
+
+function matches(source, expected) {
+  it(`${source}`, assert => {
+    let actual = transform(source);
+
+    assert.equal(actual, expected);
+  });
+}
+
+describe('ember-modules-api-polyfill', () => {
+});
