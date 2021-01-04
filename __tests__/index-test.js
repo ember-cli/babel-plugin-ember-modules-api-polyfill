2f152557b0c61c864175a57de13f2c65c34752e2
Derek Wickern <dwickern@gmail.com>
2021-01-04 10:10:56 -0800

    add failed test for using action as a variable name

--- __tests__/index-test.js
+++ __tests__/index-test.js
@@ -25,7 +25,10 @@ function transform7(source, _plugins) {
 
 function transformWithPresetEnv(source) {
   let result = babel7.transformSync(source, {
-    plugins: [[Plugin]],
+    plugins: [
+      [Plugin],
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ],
     presets: [['@babel/preset-env', { targets: { ie: '8' }, modules: false }]],
   });
 
@@ -420,5 +423,72 @@ describe('when used with typescript', () => {
     ]);
 
     expect(actual).toEqual(`Ember.addObserver();`);
+  });
+});
+
+describe('when used with native classes and decorators', () => {
+  it('allows "action" to be used as a variable name', () => {
+    let source = `
+import { action } from '@ember/object';
+import Controller from '@ember/controller';
+
+export default class MyController extends Controller {
+  @action
+  addAction(action) {
+    this.actions.pushObject(action);
+  }
+}
+`;
+
+    let actual = transformWithPresetEnv(source);
+
+    expect(actual)
+      .toEqual(`function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+var _dec, _class;
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _applyDecoratedDescriptor(target, property, decorators, descriptor, context) { var desc = {}; Object.keys(descriptor).forEach(function (key) { desc[key] = descriptor[key]; }); desc.enumerable = !!desc.enumerable; desc.configurable = !!desc.configurable; if ('value' in desc || desc.initializer) { desc.writable = true; } desc = decorators.slice().reverse().reduce(function (desc, decorator) { return decorator(target, property, desc) || desc; }, desc); if (context && desc.initializer !== void 0) { desc.value = desc.initializer ? desc.initializer.call(context) : void 0; desc.initializer = undefined; } if (desc.initializer === void 0) { Object.defineProperty(target, property, desc); desc = null; } return desc; }
+
+var MyController = (_dec = Ember._action, (_class = /*#__PURE__*/function (_Ember$Controller) {
+  _inherits(MyController, _Ember$Controller);
+
+  var _super = _createSuper(MyController);
+
+  function MyController() {
+    _classCallCheck(this, MyController);
+
+    return _super.apply(this, arguments);
+  }
+
+  _createClass(MyController, [{
+    key: "addAction",
+    value: function addAction(action) {
+      this.actions.pushObject(action);
+    }
+  }]);
+
+  return MyController;
+}(Ember.Controller), (_applyDecoratedDescriptor(_class.prototype, "addAction", [_dec], Object.getOwnPropertyDescriptor(_class.prototype, "addAction"), _class.prototype)), _class));
+export { MyController as default };`);
   });
 });
